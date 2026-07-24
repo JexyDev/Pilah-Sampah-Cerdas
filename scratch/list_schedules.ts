c1744fb8d4935cbd2e2e7e558985f575915f6d1f
Jeremy Darrell <JerDev@gmail.com>
2026-07-24 10:58:20 +0700

    feat(schedule): add delete schedule endpoint for superadmin

--- scratch/list_schedules.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-async function main() {
-  const schedules = await prisma.schedule.findMany();
-  console.log(JSON.stringify(schedules, null, 2));
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
