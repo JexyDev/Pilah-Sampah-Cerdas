68b0bc81df54baa62fafc154ba6ab74defaf22b6
Daffa Jaya Perkasa <251403746+dafayape@users.noreply.github.com>
2026-08-07 21:26:00 +0700

    fix(api): expose nim in top-level user object response

--- apps/api/src/services/userService.ts
+++ apps/api/src/services/userService.ts
@@ -168,6 +168,7 @@ export class UserService {
         name: u.name,
         email: u.phone,
         phone: u.phone,
+        nim: u.studentProfile?.nim || null,
         role: u.role.name,
         status: u.status,
         binStatus,
