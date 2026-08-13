1c44aa16345fd690d4858bab0397c16fe92ecd9d
Daffa Jaya Perkasa <251403746+dafayape@users.noreply.github.com>
2026-08-13 18:36:40 +0700

    fix(dpl): update all 32 DPLs with accurate NIP, programStudi, name corrections (Dr. Wendi Zarman, Fenny M.Hum)

--- apps/api/src/scripts/sync_real_dpl.ts
+++ apps/api/src/scripts/sync_real_dpl.ts
@@ -1,6 +1,6 @@
 /**
  * Script untuk menyelaraskan 32 Data Dosen Pembimbing Lapangan (DPL) Real
- * dengan format penamaan KKN standar: "Kelompok [Nomor] [Kelurahan]".
+ * dengan NIP, Program Studi, dan nama kelompok baku sesuai dokumen resmi UNIKOM.
  */
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
@@ -8,42 +8,42 @@ import bcrypt from "bcryptjs";
 const prisma = new PrismaClient();
 
 export const REAL_32_DPL_STANDARDIZED = [
-  { no: 1, name: "Muhammad Aksan Ipaenin, S.T. M.Sc", phone: "+6285294754801", rawPhone: "085294754801", kelompok: "Kelompok 1 Lebak Gede", kelurahan: "Lebak Gede" },
-  { no: 2, name: "Assoc.Prof. Dr. Wartika S.Kom.,MT", phone: "+62895337560201", rawPhone: "0895337560201", kelompok: "Kelompok 2 Lebak Gede", kelurahan: "Lebak Gede" },
-  { no: 3, name: "Myrna Dwi Rahmatya, S.Kom.,M.Kom", phone: "+6285320322236", rawPhone: "085320322236", kelompok: "Kelompok 3 Lebak Gede", kelurahan: "Lebak Gede" },
-  { no: 4, name: "Alif Finandhita, S.Kom., M.T.", phone: "+6282115865070", rawPhone: "082115865070", kelompok: "Kelompok 4 Lebak Gede", kelurahan: "Lebak Gede" },
-  { no: 5, name: "Adam Mukharil Bachtiar, S.Kom., M.T., Ph.D", phone: "+6281318920636", rawPhone: "081318920636", kelompok: "Kelompok 1 Sekeloa", kelurahan: "Sekeloa" },
-  { no: 6, name: "Dr. Eng. Siswanti Zuraida, S.Pd., M.T.", phone: "+6288210288162", rawPhone: "088210288162", kelompok: "Kelompok 2 Sekeloa", kelurahan: "Sekeloa" },
-  { no: 7, name: "Dr. Olih Solihin, S.Sos., M.I.Kom.", phone: "+6289656618667", rawPhone: "089656618667", kelompok: "Kelompok 3 Sekeloa", kelurahan: "Sekeloa" },
-  { no: 8, name: "Hery Dwi Yulianto, S.T., M.Kom.", phone: "+628382821127", rawPhone: "08382821127", kelompok: "Kelompok 4 Sekeloa", kelurahan: "Sekeloa" },
-  { no: 9, name: "John Adler, S.Si., M.Si.", phone: "+6282130536915", rawPhone: "082130536915", kelompok: "Kelompok 5 Sekeloa", kelurahan: "Sekeloa" },
-  { no: 10, name: "Dr. Henike Primawati, S.IP., M.I.Pol.", phone: "+628118748686", rawPhone: "08118748686", kelompok: "Kelompok 6 Sekeloa", kelurahan: "Sekeloa" },
-  { no: 11, name: "Fenny Febrianti, S.S.,M.Hum", phone: "+6282121822503", rawPhone: "082121822503", kelompok: "Kelompok 1 Lebak Siliwangi", kelurahan: "Lebak Siliwangi" },
-  { no: 12, name: "Dr. Tatik Fidowaty, S.IP., M.Si", phone: "+62817616930", rawPhone: "0817616930", kelompok: "Kelompok 2 Lebak Siliwangi", kelurahan: "Lebak Siliwangi" },
-  { no: 13, name: "Dr. Nungki Heriyati, S.S.S.,I.Kom.,M.A.", phone: "+6281322752828", rawPhone: "081322752828", kelompok: "Kelompok 3 Lebak Siliwangi", kelurahan: "Lebak Siliwangi" },
-  { no: 14, name: "Dr. Agus Mulyana, S.Kom, M.T.", phone: "+6282116871007", rawPhone: "82116871007", kelompok: "Kelompok 1 Sadang Serang", kelurahan: "Sadang Serang" },
-  { no: 15, name: "Amilia Widya, S.Pd., M.T.", phone: "+6281344706038", rawPhone: "081344706038", kelompok: "Kelompok 2 Sadang Serang", kelurahan: "Sadang Serang" },
-  { no: 16, name: "Wahyudi, S.H., M.H.", phone: "+6281321920848", rawPhone: "081321920848", kelompok: "Kelompok 3 Sadang Serang", kelurahan: "Sadang Serang" },
-  { no: 17, name: "Richi Dwi Agustia, S.Kom., M.Kom.", phone: "+6285780084003", rawPhone: "085780084003", kelompok: "Kelompok 4 Sadang Serang", kelurahan: "Sadang Serang" },
-  { no: 18, name: "Assoc. Prof., Dr. Manap Solihat, Drs., M.Si.", phone: "+6281321911449", rawPhone: "081321911449", kelompok: "Kelompok 5 Sadang Serang", kelurahan: "Sadang Serang" },
-  { no: 19, name: "Cherry Dharmawan, S.Sn., M.Sn.", phone: "+6282118047608", rawPhone: "082118047608", kelompok: "Kelompok 6 Sadang Serang", kelurahan: "Sadang Serang" },
-  { no: 20, name: "Assoc. Prof. Dr. Sri Dewi Anggadini, S.E., M.Si., Ak., CA", phone: "+628122421004", rawPhone: "08122421004", kelompok: "Kelompok 7 Sadang Serang", kelurahan: "Sadang Serang" },
-  { no: 21, name: "Dr.H.Tatang Supriyadi,S.E.,M.M", phone: "+6281222927778", rawPhone: "081222927778", kelompok: "Kelompok 8 Sadang Serang", kelurahan: "Sadang Serang" },
-  { no: 22, name: "Dr. Wendi Zaman,M.Si", phone: "+628157131405", rawPhone: "08157131405", kelompok: "Kelompok 9 Sadang Serang", kelurahan: "Sadang Serang" },
-  { no: 23, name: "Arif Try Cahyadi, S.Ds., M.Ds.", phone: "+6282298522354", rawPhone: "082298522354", kelompok: "Kelompok 10 Sadang Serang", kelurahan: "Sadang Serang" },
-  { no: 24, name: "Ayub Subandi, S.Si., M.T., Ph.D.", phone: "+6289612270264", rawPhone: "089612270264", kelompok: "Kelompok 11 Sadang Serang", kelurahan: "Sadang Serang" },
-  { no: 25, name: "Iyan Andriana, S.T., M.T.", phone: "+628112334224", rawPhone: "08112334224", kelompok: "Kelompok 1 Cipaganti", kelurahan: "Cipaganti" },
-  { no: 26, name: "Hanhan Maulana, M.Kom., Ph.D.", phone: "+6285222267759", rawPhone: "085222267759", kelompok: "Kelompok 2 Cipaganti", kelurahan: "Cipaganti" },
-  { no: 27, name: "Assoc. Prof. Dr. Rini Maulina, S.Sn., M.Sn.", phone: "+6289670059709", rawPhone: "089670059709", kelompok: "Kelompok 3 Cipaganti", kelurahan: "Cipaganti" },
-  { no: 28, name: "Rangga Sidik, S.Kom., M.Kom., M.Eng", phone: "+6285624088878", rawPhone: "085624088878", kelompok: "Kelompok 4 Cipaganti", kelurahan: "Cipaganti" },
-  { no: 29, name: "Prof Umi Narimawati,dra, S.E. M.Si.,M.pd", phone: "+6281213143636", rawPhone: "081213143636", kelompok: "Kelompok 1 Dago", kelurahan: "Dago" },
-  { no: 30, name: "Assoc Prof. Dr. Agus Riyanto S.E., M.S.i", phone: "+6285759996154", rawPhone: "085759996154", kelompok: "Kelompok 2 Dago", kelurahan: "Dago" },
-  { no: 31, name: "Assoc. Prof. Dr. Raeni Dwi Santy, S.E., M.Si., CIMA, CDMP", phone: "+6281223216029", rawPhone: "81223216029", kelompok: "Kelompok 3 Dago", kelurahan: "Dago" },
-  { no: 32, name: "Dr. Linna Ismawati, S.E., M.Si.", phone: "+6281221471617", rawPhone: "81221471617", kelompok: "Kelompok 4 Dago", kelurahan: "Dago" },
+  { no: 1, name: "Muhammad Aksan Ipaenin, S.T. M.Sc", phone: "+6285294754801", rawPhone: "085294754801", nip: "4127.99.90.268", prodi: "S1 Teknik Sipil", kelompok: "Kelompok 1 Lebak Gede", kelurahan: "Lebak Gede" },
+  { no: 2, name: "Assoc.Prof. Dr. Wartika S.Kom.,MT", phone: "+62895337560201", rawPhone: "0895337560201", nip: "4127.70.26.002", prodi: "S1 Sistem Informasi", kelompok: "Kelompok 2 Lebak Gede", kelurahan: "Lebak Gede" },
+  { no: 3, name: "Myrna Dwi Rahmatya, S.Kom.,M.Kom", phone: "+6285320322236", rawPhone: "085320322236", nip: "4127.70.26.111", prodi: "D3 Manajemen Informatika", kelompok: "Kelompok 3 Lebak Gede", kelurahan: "Lebak Gede" },
+  { no: 4, name: "Alif Finandhita, S.Kom., M.T.", phone: "+6282115865070", rawPhone: "082115865070", nip: "4127.70.06.025", prodi: "S1 Teknik Informatika", kelompok: "Kelompok 4 Lebak Gede", kelurahan: "Lebak Gede" },
+  { no: 5, name: "Adam Mukharil Bachtiar, S.Kom., M.T., Ph.D", phone: "+6281318920636", rawPhone: "081318920636", nip: "4127.70.06.024", prodi: "S1 Teknik Informatika", kelompok: "Kelompok 1 Sekeloa", kelurahan: "Sekeloa" },
+  { no: 6, name: "Dr. Eng. Siswanti Zuraida, S.Pd., M.T.", phone: "+6288210288162", rawPhone: "088210288162", nip: "4127.88.80.717", prodi: "S1 Teknik Arsitektur", kelompok: "Kelompok 2 Sekeloa", kelurahan: "Sekeloa" },
+  { no: 7, name: "Dr. Olih Solihin, S.Sos., M.I.Kom.", phone: "+6289656618667", rawPhone: "089656618667", nip: "4127.35.30.016", prodi: "S1 Ilmu Komunikasi", kelompok: "Kelompok 3 Sekeloa", kelurahan: "Sekeloa" },
+  { no: 8, name: "Hery Dwi Yulianto, S.T., M.Kom.", phone: "+628382821127", rawPhone: "08382821127", nip: "4127.70.67.004", prodi: "D3 Komputerisasi Akuntansi", kelompok: "Kelompok 4 Sekeloa", kelurahan: "Sekeloa" },
+  { no: 9, name: "John Adler, S.Si., M.Si.", phone: "+6282130536915", rawPhone: "082130536915", nip: "4127.70.05.007", prodi: "D3 Teknik Komputer", kelompok: "Kelompok 5 Sekeloa", kelurahan: "Sekeloa" },
+  { no: 10, name: "Dr. Henike Primawati, S.IP., M.I.Pol.", phone: "+628118748686", rawPhone: "08118748686", nip: "4127.35.32.011", prodi: "S1 Hubungan Internasional", kelompok: "Kelompok 6 Sekeloa", kelurahan: "Sekeloa" },
+  { no: 11, name: "Fenny Febrianti, S.S., M.Hum", phone: "+6282121822503", rawPhone: "082121822503", nip: "4127.20.04.004", prodi: "S1 Sastra Jepang", kelompok: "Kelompok 1 Lebak Siliwangi", kelurahan: "Lebak Siliwangi" },
+  { no: 12, name: "Dr. Tatik Fidowaty, S.IP., M.Si", phone: "+62817616930", rawPhone: "0817616930", nip: "4127.35.31.009", prodi: "S1 Ilmu Pemerintahan", kelompok: "Kelompok 2 Lebak Siliwangi", kelurahan: "Lebak Siliwangi" },
+  { no: 13, name: "Dr. Nungki Heriyati, S.S.S.,I.Kom.,M.A.", phone: "+6281322752828", rawPhone: "081322752828", nip: "4127.20.03.020", prodi: "S1 Sastra Inggris", kelompok: "Kelompok 3 Lebak Siliwangi", kelurahan: "Lebak Siliwangi" },
+  { no: 14, name: "Dr. Agus Mulyana, S.Kom, M.T.", phone: "+6282116871007", rawPhone: "82116871007", nip: "4127.70.05.017", prodi: "D3 Teknik Komputer", kelompok: "Kelompok 1 Sadang Serang", kelurahan: "Sadang Serang" },
+  { no: 15, name: "Amilia Widya, S.Pd., M.T.", phone: "+6281344706038", rawPhone: "081344706038", nip: "4127.70.17.015", prodi: "S1 Teknik Perencanaan Wilayah dan Kota", kelompok: "Kelompok 2 Sadang Serang", kelurahan: "Sadang Serang" },
+  { no: 16, name: "Wahyudi, S.H., M.H.", phone: "+6281321920848", rawPhone: "081321920848", nip: "4127.33.00.019", prodi: "S1 Ilmu Hukum", kelompok: "Kelompok 3 Sadang Serang", kelurahan: "Sadang Serang" },
+  { no: 17, name: "Richi Dwi Agustia, S.Kom., M.Kom.", phone: "+6285780084003", rawPhone: "085780084003", nip: "4127.70.06.132", prodi: "S1 Teknik Informatika", kelompok: "Kelompok 4 Sadang Serang", kelurahan: "Sadang Serang" },
+  { no: 18, name: "Assoc. Prof., Dr. Manap Solihat, Drs., M.Si.", phone: "+6281321911449", rawPhone: "081321911449", nip: "4127.35.30.007", prodi: "S1 Ilmu Komunikasi", kelompok: "Kelompok 5 Sadang Serang", kelurahan: "Sadang Serang" },
+  { no: 19, name: "Cherry Dharmawan, S.Sn., M.Sn.", phone: "+6282118047608", rawPhone: "082118047608", nip: "4127.32.04.002", prodi: "S1 Desain Interior", kelompok: "Kelompok 6 Sadang Serang", kelurahan: "Sadang Serang" },
+  { no: 20, name: "Assoc. Prof. Dr. Sri Dewi Anggadini, S.E., M.Si., Ak., CA", phone: "+628122421004", rawPhone: "08122421004", nip: "4127.34.03.003", prodi: "S1 Akuntansi", kelompok: "Kelompok 7 Sadang Serang", kelurahan: "Sadang Serang" },
+  { no: 21, name: "Dr.H.Tatang Supriyadi,S.E.,M.M", phone: "+6281222927778", rawPhone: "081222927778", nip: "4127.34.02.075", prodi: "D3 Manajemen Pemasaran", kelompok: "Kelompok 8 Sadang Serang", kelurahan: "Sadang Serang" },
+  { no: 22, name: "Dr. Wendi Zarman, M.Si", phone: "+628157131405", rawPhone: "08157131405", nip: "4127.70.05.010", prodi: "S1 Sistem Komputer", kelompok: "Kelompok 9 Sadang Serang", kelurahan: "Sadang Serang" },
+  { no: 23, name: "Arif Try Cahyadi, S.Ds., M.Ds.", phone: "+6282298522354", rawPhone: "082298522354", nip: "4127.32.06.087", prodi: "S1 Desain Komunikasi Visual", kelompok: "Kelompok 10 Sadang Serang", kelurahan: "Sadang Serang" },
+  { no: 24, name: "Ayub Subandi, S.Si., M.T., Ph.D.", phone: "+6289612270264", rawPhone: "089612270264", nip: "4127.70.05.030", prodi: "S1 Teknik Elektro", kelompok: "Kelompok 11 Sadang Serang", kelurahan: "Sadang Serang" },
+  { no: 25, name: "Iyan Andriana, S.T., M.T.", phone: "+628112334224", rawPhone: "08112334224", nip: "4127.70.03.009", prodi: "S1 Teknik Industri", kelompok: "Kelompok 1 Cipaganti", kelurahan: "Cipaganti" },
+  { no: 26, name: "Hanhan Maulana, M.Kom., Ph.D.", phone: "+6285222267759", rawPhone: "085222267759", nip: "4127.70.06.134", prodi: "S1 Teknik Informatika", kelompok: "Kelompok 2 Cipaganti", kelurahan: "Cipaganti" },
+  { no: 27, name: "Assoc. Prof. Dr. Rini Maulina, S.Sn., M.Sn.", phone: "+6289670059709", rawPhone: "089670059709", nip: "4127.32.06.011", prodi: "D3 Desain Grafis", kelompok: "Kelompok 3 Cipaganti", kelurahan: "Cipaganti" },
+  { no: 28, name: "Rangga Sidik, S.Kom., M.Kom., M.Eng", phone: "+6285624088878", rawPhone: "085624088878", nip: "4127.70.26.113", prodi: "S1 Sistem Informasi", kelompok: "Kelompok 4 Cipaganti", kelurahan: "Cipaganti" },
+  { no: 29, name: "Prof Umi Narimawati,dra, S.E. M.Si.,M.pd", phone: "+6281213143636", rawPhone: "081213143636", nip: "4127.34.02.015", prodi: "S1 Manajemen", kelompok: "Kelompok 1 Dago", kelurahan: "Dago" },
+  { no: 30, name: "Assoc Prof. Dr. Agus Riyanto S.E., M.S.i", phone: "+6285759996154", rawPhone: "085759996154", nip: "4127.70.03.007", prodi: "S1 Manajemen", kelompok: "Kelompok 2 Dago", kelurahan: "Dago" },
+  { no: 31, name: "Assoc. Prof. Dr. Raeni Dwi Santy, S.E., M.Si., CIMA, CDMP", phone: "+6281223216029", rawPhone: "81223216029", nip: "4127.34.02.006", prodi: "S1 Manajemen", kelompok: "Kelompok 3 Dago", kelurahan: "Dago" },
+  { no: 32, name: "Dr. Linna Ismawati, S.E., M.Si.", phone: "+6281221471617", rawPhone: "81221471617", nip: "4127.34.02.008", prodi: "S1 Manajemen", kelompok: "Kelompok 4 Dago", kelurahan: "Dago" },
 ];
 
 async function syncDpl() {
-  console.log("=== SINKRONISASI 32 DATA DPL & KELOMPOK STANDAR ('Kelompok [X] [Kelurahan]') ===");
+  console.log("=== SINKRONISASI 32 DATA DPL & KELOMPOK STANDAR (NIP + PRODI) ===");
 
   const dplRole = await prisma.role.findUnique({ where: { name: "DPL" } });
   if (!dplRole) {
@@ -54,7 +54,6 @@ async function syncDpl() {
   const defaultPass = await bcrypt.hash("password123", 10);
 
   for (const item of REAL_32_DPL_STANDARDIZED) {
-    // 1. Cari user berdasarkan nomor HP persis
     let existingUser = await prisma.user.findFirst({
       where: {
         OR: [
@@ -80,32 +79,35 @@ async function syncDpl() {
         data: {
           name: item.name,
           phone: item.phone,
+          nip: item.nip,
+          programStudi: item.prodi,
           roleId: dplRole.id,
           status: "Aktif",
           institusi: "Universitas Komputer Indonesia",
         },
       });
-      console.log(`[UPDATED USER #${item.no}] ${item.name} (${item.phone})`);
+      console.log(`[UPDATED USER #${item.no}] ${item.name} | NIP: ${item.nip} | Prodi: ${item.prodi}`);
     } else {
       try {
         existingUser = await prisma.user.create({
           data: {
             name: item.name,
             phone: item.phone,
+            nip: item.nip,
+            programStudi: item.prodi,
             password: defaultPass,
             roleId: dplRole.id,
             status: "Aktif",
             institusi: "Universitas Komputer Indonesia",
           },
         });
-        console.log(`[CREATED USER #${item.no}] ${item.name} (${item.phone})`);
+        console.log(`[CREATED USER #${item.no}] ${item.name} | NIP: ${item.nip} | Prodi: ${item.prodi}`);
       } catch (err) {
         console.warn(`[SKIP CREATE USER #${item.no}] Phone constraint: ${item.phone}`);
         continue;
       }
     }
 
-    // 2. Hubungkan atau buat Kelompok KKN dengan format standar
     if (existingUser) {
       let existingKel = await prisma.kelompokKkn.findFirst({
         where: {
@@ -123,7 +125,6 @@ async function syncDpl() {
             kelurahan: item.kelurahan,
           },
         });
-        console.log(`  └─ [LINKED STANDARDIZED KELOMPOK] ${item.kelompok} -> DPL: ${item.name}`);
       } else {
         await prisma.kelompokKkn.create({
           data: {
@@ -133,12 +134,11 @@ async function syncDpl() {
             kelurahan: item.kelurahan,
           },
         });
-        console.log(`  └─ [CREATED STANDARDIZED KELOMPOK] ${item.kelompok} -> DPL: ${item.name}`);
       }
     }
   }
 
-  console.log("=== SINKRONISASI 32 DPL STANDAR SELESAI ===");
+  console.log("=== SINKRONISASI 32 DPL SELESAI ===");
 }
 
 syncDpl()
