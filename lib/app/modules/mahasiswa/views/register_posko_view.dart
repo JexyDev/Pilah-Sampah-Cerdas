d3d1d7d6d477493e106be3cafdf006f7805261f1
muhamadhabilputrawan <muhamadhabilputrawan@gmail.com>
2026-08-19 05:27:23 +0700

    fix:typo import register posko

--- lib/app/modules/mahasiswa/views/register_posko_view.dart
+++ lib/app/modules/mahasiswa/views/register_posko_view.dart
@@ -1,4 +1,4 @@
-giimport 'dart:io';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:geolocator/geolocator.dart';
